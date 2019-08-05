52431a2d4c01de3472f0f00f102f9db5ca3efb31
Wern Ancheta <ancheta.wern@gmail.com>
2019-08-05 22:21:59 +0800

    initialize project

---
+++ app/components/App.vue
@@ -0,0 +1,98 @@
+<template>
+  <Page>
+    <ActionBar title="NativeScript-Vue Pokedex"/>
+    <StackLayout backgroundColor="#3c495e">
+      <ListView class="list-group" for="pokemon in pokedex" separatorColor="#e6e6e6" @itemTap="onItemTap">
+        <v-template>
+          <FlexboxLayout flexDirection="row" class="list-group-item">
+            <Image :src="pokemon.sprite" class="thumb" />
+            <Label :text="pokemon.name" class="list-group-item-heading" style="width: 60%" />
+          </FlexboxLayout>
+        </v-template>
+      </ListView>
+    </StackLayout>
+  </Page>
+</template>
+
+<script>
+import axios from 'axios/dist/axios';
+import Pokemon from './Pokemon';
+
+const POKE_API_BASE_URL =  "https://pokeapi.co/api/v2";
+
+export default {
+
+  async mounted() {
+    const { data } = await axios.get('https://pokeapi.co/api/v2/pokemon/?limit=150');
+    this.pokedex = data.results.map((item) => {
+      const id = item.url.split('/')[6];
+      return {
+        id,
+        name: item.name,
+        sprite: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`
+      }
+    });
+
+  },
+
+  data() {
+    return {
+      pokedex: []
+    }
+  },
+
+  methods: {
+    onItemTap: async function (args) {
+      const id = args.item.id;
+      const { data: pokemonData } = await axios.get(`${POKE_API_BASE_URL}/pokemon/${id}`);
+      const { data: pokemonSpecieData } = await axios.get(`${POKE_API_BASE_URL}/pokemon-species/${id}`);
+
+      const { types } = pokemonData;
+      const { flavor_text_entries } = pokemonSpecieData;
+
+      const pokemon_types = types.map(({ slot, type }) => {
+        return {
+          "id": slot,
+          "name": type.name,
+          "classname": `TypeText ${type.name}`
+        }
+      });
+
+      const pokemon_description = flavor_text_entries.find(item => item.language.name === 'en').flavor_text;
+
+      this.$navigateTo(Pokemon, {
+        props: {
+          id,
+          name: args.item.name,
+          description: pokemon_description,
+          types: pokemon_types,
+          image: `https://img.pokemondb.net/artwork/vector/large/${args.item.name}.png`
+        }
+      });
+    },
+  },
+
+}
+</script>
+
+<style scoped>
+ActionBar {
+  background-color: #53ba82;
+  color: #ffffff;
+}
+
+.list-group-item {
+  background-color: #FFF;
+  align-items: center;
+}
+
+.thumb {
+  width: 100;
+}
+
+.list-group-item-heading {
+  color: #333;
+  font-size: 20;
+  margin-left: 20;
+}
+</style>
---
+++ app/components/Pokemon.vue
@@ -0,0 +1,151 @@
+<template>
+  <Page>
+    <ActionBar title="NativeScript-Vue Pokemon"/>
+    <StackLayout backgroundColor="#FFF">
+      <FlexboxLayout flexDirection="column" class="PokemonContainer">
+
+        <Image :src="this.image" />
+        <Label :text="this.name" class="PokemonName" />
+
+        <ListView class="PokemonTypes" separatorColor="#FFF" for="type in this.types">
+          <v-template>
+            <Label :text="type.name" :class="type.classname" />
+          </v-template>
+        </ListView>
+
+        <Label :text="this.description" class="PokemonDescription" textWrap="true" />
+
+        <Button text="Back" @tap="goBack" />
+
+      </FlexboxLayout>
+    </StackLayout>
+  </Page>
+</template>
+
+<script>
+  export default {
+
+    props: ['id', 'name', 'image', 'description', 'types'],
+
+    methods: {
+      goBack: function (args) {
+        this.$navigateBack();
+      }
+    }
+
+  }
+</script>
+
+<style scoped>
+ActionBar {
+  background-color: #53ba82;
+  color: #ffffff;
+}
+
+.PokemonContainer {
+  align-items: center;
+}
+
+Image {
+  width: 270;
+  margin-top: 20;
+}
+
+.PokemonName {
+  font-size: 35;
+  color: #333;
+}
+
+.PokemonDescription {
+  padding: 10;
+  font-size: 16;
+}
+
+.PokemonTypes {
+  margin-top: 10;
+}
+
+.TypeText {
+  text-align: center;
+  width: 75;
+  color: #FFF;
+  padding: 5;
+}
+
+.poison {
+  background-color: violet;
+}
+
+.normal {
+  background-color: #8a8a59;
+}
+
+.fire {
+  background-color: #f08030;
+}
+
+.water {
+  background-color: #6890f0;
+}
+
+.electric {
+  background-color: #f8d030;
+}
+
+.grass {
+  background-color: #78c850;
+}
+
+.ice {
+  background-color: #98d8d8;
+}
+
+.fighting {
+  background-color: #c03028;
+}
+
+.poison {
+  background-color: #a040a0;
+  color: #FFF;
+}
+
+.ground {
+  background-color: #e0c068;
+}
+
+.flying {
+  background-color: #a890f0;
+}
+
+.psychic {
+  background-color: #f85888;
+}
+
+.bug {
+  background-color: #a8b820;
+}
+
+.rock {
+  background-color: #b8a038;
+}
+
+.ghost {
+  background-color: #705898;
+}
+
+.dragon {
+  background-color: #7038f8;
+}
+
+.dark {
+  background-color: #705848;
+}
+
+.steel {
+  background-color: #b8b8d0;
+}
+
+.fairy {
+  background-color: #e898e8;
+}
+</style>
